f9d46f1663c5c6d6bfe130ee49e612cce06276f0
CelineBarbe <laloutrecosmique@hotmail.fr>
2022-01-18 15:04:27 +0100

    authentification via oauth google pour récupérer quelques données personnalisées

--- pageAccueil/popup.js
+++
@@ -1,7 +0,0 @@
-// action effective à l'appel du bouton de l'extension
-function openNewTab() {
-    var newURL = "page.html";
-    chrome.tabs.create({ url: newURL });
-}
-
-document.getElementById("open").addEventListener("click", openNewTab);
